32950bc59c9313aa16f12b94fd56c8d5f4ad86f9
112RifqiAlBukhari <rifqialbukhari32@gmail.com>
2024-10-23 16:15:02 +0800

    pertemuan 8

---
+++ pertemuan 8/2409106112_rifqi al bukhari_PERTEMUAN 8.py
@@ -0,0 +1,36 @@
+# angka = int(input("Masukkan angka: "))
+# print(angka)
+# #input “Hello”
+
+# IndexError
+# angka = [1,2,3,4]
+# print(angka[5])
+
+# KeyError
+# sosmed = {
+#     "ig" : "@rifqi",
+#     "fb" : "rifqi"
+# }
+
+# print(sosmed["wa"])
+
+# try:
+#     angka = int(input("Masukkan angka: "))
+# except ValueError:
+#     print("Input yang anda masukkan bukan angka")
+# except IndexError:
+#     print("Input yang anda masukkan lebig dari range")
+
+# try:
+#     nama = input("Hello, what's your name? ")
+#     if len(nama) > 5:
+#         raise ValueError("Nama tidak boleh lebih dari 5karakter")
+# except ValueError as e:
+#     print(e)
+
+# nama = input("masukkan nama :")
+# namabaru = nama.capitalize()
+
+# print(namabaru)
+
+
